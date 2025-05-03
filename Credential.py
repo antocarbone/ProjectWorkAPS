6385c3ababdbf332c1e3ebae5734c1b814f15799
antocarbone <antonio_crb@outlook.it>
2025-05-03 17:22:57 +0200

    metodo toJSON della classe Certificate testato

--- Credential.py
+++ Credential.py
@@ -1,89 +1,25 @@
-from abc import ABC, abstractmethod
+from Utils import *
+import json
 
-class Issuer:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-    
-    def __str__(self):
-        return self.id+self.name 
-        
-class Owner:
-    def __init__(self, name, surname, cf, email, birthDate):
-        self.name = name
-        self.surname = surname
-        self.cf = cf
-        self.email = email
-        self.birthDate = birthDate
-        
-    def __str__(self):
-        return self.name+self.surname+self.cf+self.email+self.birthDate
-
-class Property(ABC):
-    @abstractmethod
-    def toString(self):
-        pass
-    
-class HiddenProperty(Property):
-    def __init__(self, hash:str):
-        self.hash = hash
-    
-    def toString(self):
-        return self.hash
-    
-class ErasmusInfo(Property):
-    def __init__(self, programName:str, startActivity:str, endActivity:str):
-        self.programName = programName
-        self.startActivity = startActivity
-        self.endActivity = endActivity
-        
-    def toString(self):
-        return self.programName+self.startActivity+self.endActivity
-    
-class Course(Property):
-    def __init__(self, name:str, achieved:bool, grade:int, cfu:int, achievementData:str):
-        self.name = name
-        self.achieved = achieved
-        self.grade = grade
-        self.cfu = cfu
-        self.achievementData = achievementData
-        
-    def toString(self):
-        return self.name+str(self.achieved)+str(self.grade)+str(self.cfu)+self.achievementData
-
-class ExtraActivity(Property):
-    def __init__(self, name:str, cfu:int):
-        self.name = name
-        self.cfu = cfu
-
-    def toString(self):
-        return self.name+str(self.cfu)
-     
-class Residence(Property):
-    def __init__(self, typology:str, address:str):
-        self.typology = typology
-        self.address = address
-        
-    def toString(self):
-        return self.typology+self.address
-
-class Scholarship(Property):
-    def __init__(self, amount:float, unit:str, payments:int):
-        self.amount = amount
-        self.unit = unit
-        self.payments = payments
-    
-    def toString(self):
-        return str(self.amount)+self.unit+str(self.payments)
-            
-class Credential:
-    def __init__(self, credentialId:str, issuranceDate:str, issuerData:Issuer, ownerData:Owner, properties:list):
-        self.credentialId = credentialId
+class Certificate:
+    def __init__(self, certificateId:str, issuranceDate:str, issuerInfo:Issuer, credentialSubject:Owner, properties:list):
+        self.certificateId = certificateId
         self.issuranceDate = issuranceDate
-        self.issuerData = issuerData
-        self.ownerData = ownerData
+        self.issuerInfo = issuerInfo
+        self.credentialSubject = credentialSubject
         self.properties = properties
     
-    def release():
+    def toJSON(self):
+        outJson = {}
+        outJson["certificateId"] = self.certificateId
+        outJson["issuranceDate"] = self.issuranceDate
+        outJson["issuerInfo"] = self.issuerInfo.__dict__
+        outJson["credentialSubject"] = self.credentialSubject.__dict__
+        propertiesJSON = []
+        for property in self.properties:
+            propertiesJSON.append(property.toDict())
+        outJson["properties"] = propertiesJSON
+        return json.dumps(outJson)
+    
+    def fromJSON(json):
         pass
-        
--- main.py
+++ main.py
@@ -2,14 +2,5 @@ from Credential import *
 from MerkleTree import *
 from hashlib import sha256
 
-testLeaves = [
-    ErasmusInfo('Erasmus+', '28-09-2026', '28-12-2026'), 
-    HiddenProperty(sha256(b'ciao').hexdigest()),
-    Course('Analisi 2', True, 27, 9, '02-08-2022')
-    ]
-
-tree = MerkleTree(testLeaves)
-for elem in testLeaves:
-    print(sha256(elem.toString().encode()).hexdigest())
-print(tree.root)
-print(tree.treeLeaves)
+credential = Certificate('1234567890', '01-09-2026', Issuer('1234', 'UNISA'), Owner('Antonio', 'Carbone', 'CRBNTN02S29H163Q', 'a.carbone88@studenti.unisa.it', '29-11-2002'), [Course('Alanizee 2', True, 21, 12, '12-11-2026')])
+print(credential.toJSON())
